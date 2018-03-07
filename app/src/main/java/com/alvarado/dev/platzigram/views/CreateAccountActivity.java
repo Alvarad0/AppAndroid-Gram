9148107170dbdfe331646d95a761cf7de3147650
Alvarad0 <alvarado.programador@gmail.com>
2018-03-07 15:55:04 -0600

    Interface Inicio de Sesión maquetada

--- app/src/main/java/com/alvarado/dev/platzigram/views/CreateAccountActivity.java
+++ app/src/main/java/com/alvarado/dev/platzigram/views/CreateAccountActivity.java
@@ -1,18 +1,16 @@
-package com.alvarado.dev.platzigram;
+package com.alvarado.dev.platzigram.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class LoginActivity extends AppCompatActivity {
+import com.alvarado.dev.platzigram.R;
+
+public class CreateAccountActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-    }
-
-    public void crearCuenta(View view){
-
+        setContentView(R.layout.activity_create_account);
     }
 }
