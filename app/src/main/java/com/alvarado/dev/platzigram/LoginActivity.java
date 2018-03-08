df6fd5c76b7d56dcd8aa9d6fbcc5f57d2eb41a8f
Alvarad0 <alvarado.programador@gmail.com>
2018-03-08 06:54:47 -0600

    Interfaz Crear Cuenta maquetada Funcionalidad de enlace de interfaces y botón atras en el toolbar

--- app/src/main/java/com/alvarado/dev/platzigram/LoginActivity.java
+++ app/src/main/java/com/alvarado/dev/platzigram/LoginActivity.java
@@ -1,8 +1,11 @@
 package com.alvarado.dev.platzigram;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import com.alvarado.dev.platzigram.views.CreateAccountActivity;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -13,6 +16,8 @@ public class LoginActivity extends AppCompatActivity {
     }
 
     public void crearCuenta(View view){
+        Intent intent = new Intent(this, CreateAccountActivity.class);
+        startActivity(intent);
 
     }
 }
--- app/src/main/java/com/alvarado/dev/platzigram/views/CreateAccountActivity.java
+++ app/src/main/java/com/alvarado/dev/platzigram/views/CreateAccountActivity.java
@@ -2,6 +2,7 @@ package com.alvarado.dev.platzigram.views;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.alvarado.dev.platzigram.R;
@@ -12,5 +13,13 @@ public class CreateAccountActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_account);
+        mostrarToolbar(getResources().getString(R.string.txtToolBar), true);
+    }
+
+    public void mostrarToolbar(String titulo, boolean botonAtras){
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle(titulo);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(botonAtras);
     }
 }
