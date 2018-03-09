a80de471ce0d2c64166fd1138af1903b3f2a0d17
Alvarad0 <alvarado.programador@gmail.com>
2018-03-09 10:25:15 -0600

    Maquetación de BottonTabs

--- app/src/main/java/com/alvarado/dev/platzigram/LoginActivity.java
+++ app/src/main/java/com/alvarado/dev/platzigram/LoginActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.alvarado.dev.platzigram.views.ContainerActivity;
 import com.alvarado.dev.platzigram.views.CreateAccountActivity;
 
 public class LoginActivity extends AppCompatActivity {
@@ -19,5 +20,10 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = new Intent(this, CreateAccountActivity.class);
         startActivity(intent);
 
+    }
+
+    public void paginaInicio(View view){
+        Intent intent = new Intent(this, ContainerActivity.class);
+        startActivity(intent);
     }
 }
